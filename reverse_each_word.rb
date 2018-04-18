f9eff345ad3ac8300279a7b0373e4e5b8cc1f7d4
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-18 20:25:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(strings)
   string = strings.split(" ")
   string.each { |strin| strin.reverse! }
-  puts string.join(" ")
+  string.join(" ")
   
 end 
 #[1, 2, 3, 4, 5].each { |number| puts "#{number} was passed to the block" }
